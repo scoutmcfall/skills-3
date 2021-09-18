21d2a5061b5fe3935c321e3203d5d21cdc739f7e
Scout McFall <scoutmknz@gmail.com>
2021-09-18 18:36:01 +0000

    2 tests passed, 9 failed

--- server.py
+++ server.py
@@ -36,6 +36,34 @@ MOST_LOVED_MELONS = {
 
 
 # TODO: replace this comment with your code
+@app.route("/")
+def index():
+    """Return homepage."""
+
+    return render_template("homepage.html")
+
+@app.route("/top-melons")
+def top_melons():
+    """Return page showing the most loved melons"""
+
+    return render_template("top-melons.html",
+                           most_loved_melons = MOST_LOVED_MELONS)
+
+@app.route("/login", methods=["POST"])
+def process_login():
+    """Collect user name
+
+    Find the user's login credentials located in the 'request.form'
+    dictionary, look up the user, and store them in the session.
+    """
+    session[name] = name
+    flash ("we got your name!")
+   
+    name = request.form.get("name")
+    session[name] = name
+    flash ("we got your name!")
+    name_list = []
+    return redirect("/top-melons")
 
 
 if __name__ == "__main__":
